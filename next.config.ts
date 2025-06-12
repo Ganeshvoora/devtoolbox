1a192016aa4b3632d094c4572dc0b794fca25466
Ganeshvoora <venkatasaiganeshvoora@gmail.com>
2025-06-12 11:50:48 +0530

    prisma fixed

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-// eslint:{
-//   ignoreDuringBuilds: true
-// },
-// typescript:{
-//   ignoreBuildErrors: true
-// }
+  ignores: ['app/generated/prisma/**'],
 };
 
 export default nextConfig;
