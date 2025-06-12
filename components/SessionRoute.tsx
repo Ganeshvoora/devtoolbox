5d88f0373a97d7ba8e206488e175317f159336a4
Ganeshvoora <venkatasaiganeshvoora@gmail.com>
2025-06-12 16:12:00 +0530

    blank render fixed

--- components/SessionRoute.tsx
+++ components/SessionRoute.tsx
@@ -1,28 +1,15 @@
 "use client"
-// import React from 'react'
-// import { SessionProvider } from "next-auth/react";
+import React from 'react'
+import { SessionProvider } from "next-auth/react";
 
-// const SessionRoute = ({children}: {
-//   children: React.ReactNode;
-// }) => {
-//   return (
-//     <SessionProvider>
-//       {children}
-//     </SessionProvider>
-//   )
-// }
-
-// export default SessionRoute
-// Inside SessionRoute.tsx
-import { usePathname } from "next/navigation";
-
-const PUBLIC_ROUTES = ["/signin", "/signup","/"];
-
-export default function SessionRoute({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  if (PUBLIC_ROUTES.includes(pathname)) {
-    return <>{children}</>;
-  }
-  
-  // ...existing auth logic...
+const SessionRoute = ({children}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
 }
+
+export default SessionRoute
